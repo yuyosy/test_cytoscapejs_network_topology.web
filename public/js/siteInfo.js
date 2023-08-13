c09a4c195791ac777f7a6c94ba4bb4bcbe3b63a0
yuyosy <y-ysss@outlook.com>
2023-08-14 02:05:41 +0900

    Update: add data files

---
+++ public/js/siteInfo.js
@@ -0,0 +1,32 @@
+export const siteInfo = {
+  'site-tenant': {
+    name: 'Tenant',
+    order: 1,
+    priority: 0,
+  },
+  'site-tenant-aaa': {
+    name: 'Tenant-A',
+    order: 1,
+    priority: 1,
+  },
+  'site-tenant-bbb': {
+    name: 'Tenant-B',
+    order: 1,
+    priority: 2,
+  },
+  'site-internal-tenant': {
+    name: 'Internal-Tenant',
+    order: 2,
+    priority: 1,
+  },
+  'site-internal-cloud': {
+    name: 'Internal-Cloud',
+    order: 3,
+    priority: 1,
+  },
+  'site-cloud': {
+    name: 'Cloud',
+    order: 4,
+    priority: 1,
+  },
+};
